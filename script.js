5ab94218c5ed19e73f97cdc1280ec4e3d21e11b9
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2026-02-21 18:49:42 -0300

    add linhas para interagir com html onvo de carta escritas

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+const images = document.querySelectorAll(".fotos-grid img, .carta-escrita img");
+
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 
-// abrir imagem
-document.querySelectorAll(".fotos-grid img").forEach(img => {
+let index = 0;
+let scale = 1;
+
+images.forEach((img, i) => {
     img.addEventListener("click", () => {
+        index = i;
+        showImage();
         lightbox.style.display = "flex";
-        lightboxImg.src = img.src;
     });
 });
 
-// fechar ao clicar fora
-lightbox.addEventListener("click", () => {
-    lightbox.style.display = "none";
+function showImage(){
+    lightboxImg.src = images[index].src;
+    scale = 1;
+    lightboxImg.style.transform = `scale(1)`;
+}
+
+// fechar
+lightbox.addEventListener("click", ()=>{
+    lightbox.style.display="none";
+});
+
+// zoom scroll
+lightboxImg.addEventListener("wheel", (e)=>{
+    e.preventDefault();
+    scale += e.deltaY * -0.001;
+    scale = Math.min(Math.max(.8, scale), 3);
+    lightboxImg.style.transform = `scale(${scale})`;
 });
